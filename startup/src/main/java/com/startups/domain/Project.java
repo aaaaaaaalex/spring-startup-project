7a6cba9b1ed221afdcf19c9fabaa73a2fd846089
Alex O R <alex.oregan@teamwork.com>
2018-12-07 16:43:12 +0000

    cleanup domain.project:     give object an ID,     make class vars private

--- startup/src/main/java/com/startups/domain/Project.java
+++ startup/src/main/java/com/startups/domain/Project.java
@@ -2,72 +2,112 @@ package com.startups.domain;
 
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.OneToMany;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
 public class Project {
 
+
+	
+	// ------------------------------------ Class Variables
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private int projId;
+	
 	@NotNull
 	@Column
-	public String projName;
+	private String projName;
+	
 	@Column
-	public String description;
+	private String description;
+	
 	@NotNull
 	@Column
-	public double targetAmount;
-	public Date doc;
+	private double targetAmount;
+	
+	private Date doc;
+	
 	@Column
-	public boolean active;
+	private boolean active;
 	
 	@OneToMany
-	public List<Pledge> pledges;
+	private List<Pledge> pledges;
 	
 	
-	public Project(String projName, String description, double targetAmount, Date doc, boolean active) {
+	
+	// ------------------------------------ Constructor
+	public Project(int projId, String projName, String description, double targetAmount, Date doc, boolean active) {
 		super();
+		this.projId = projId;
 		this.projName = projName;
 		this.description = description;
 		this.targetAmount = targetAmount;
 		this.doc = doc;
 		this.active = active;
 	}
-	
 	public Project() {}
+
 	
+	
+	// ------------------------------------ Get / Set
 	public String getProjName() {
 		return projName;
 	}
 	public void setProjName(String projName) {
 		this.projName = projName;
 	}
+	
+	
+	public int getProjId() {
+		return projId;
+	}
+	public void setProjId(int projId) {
+		this.projId = projId;
+	}
+	
+	
+	public List<Pledge> getPledges() {
+		return pledges;
+	}
+	public void setPledges(List<Pledge> pledges) {
+		this.pledges = pledges;
+	}
+	
+	
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	
 	public double getTargetAmount() {
 		return targetAmount;
 	}
 	public void setTargetAmount(double targetAmount) {
 		this.targetAmount = targetAmount;
 	}
+	
+	
 	public Date getDoc() {
 		return doc;
 	}
 	public void setDoc(Date doc) {
 		this.doc = doc;
 	}
+	
+	
 	public boolean isActive() {
 		return active;
 	}
 	public void setActive(boolean active) {
 		this.active = active;
 	}
-	
-	
 }
