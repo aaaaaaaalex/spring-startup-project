a770f8db9f3505c78003b4bfe441412510ed62b9
Alex O R <alex.oregan@teamwork.com>
2018-12-08 18:04:34 +0000

    add websecurityconfig file, styles, and webpages login, 403, and register

---
+++ startup/src/main/java/com/startups/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.startups.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	
+	
+	// Authentication configuration for requests
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+			.antMatchers(
+					"/", "/css/**" , "/allprojects", "/projectdetails") .permitAll() //unauthenticated pages
+			
+			.anyRequest().authenticated()
+			
+			.and().formLogin().loginPage("/login").permitAll() // custom login and 403 pages
+					.defaultSuccessUrl("/dashboard") // dashboard is the user's overview page
+					.usernameParameter("email") // the user logs in with their email, not a username
+			.and().exceptionHandling().accessDeniedPage("/403"); 
+	}
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		String encodedPassword = this.passwordEncoder.encode("password");
+		
+		auth.inMemoryAuthentication() // this is only auth'ing based on a generic user with name "user" and password "pasword"
+			.withUser("user").password(encodedPassword).roles("USER").and()
+			.withUser("admin").password(encodedPassword).roles("USER","ADMIN");
+	}
+	
+	
+	
+}
---
+++ startup/src/main/java/com/startups/controllers/LoginController.java
@@ -0,0 +1,32 @@
+package com.startups.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	HttpSession session;
+	
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping(value = "/logout")
+	public String logout() {
+		session.removeAttribute("loggedin");
+		return "/index";
+	}
+	
+	
+	@GetMapping(value = "/403")
+	public String accessDenied() {
+		return "403";
+	}
+	
+}
