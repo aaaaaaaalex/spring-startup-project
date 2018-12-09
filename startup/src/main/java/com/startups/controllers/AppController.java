6cc8bc53ffb2f4b1f8fb795899ad87cc8f453f32
Raddish18 <cianr22@hotmail.com>
2018-12-09 23:32:14 +0000

    Add Project (Broken)

--- startup/src/main/java/com/startups/controllers/AppController.java
+++ startup/src/main/java/com/startups/controllers/AppController.java
@@ -2,10 +2,19 @@ package com.startups.controllers;
 
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.startups.domain.Project;
+import com.startups.services.ProjectService;
 
 // "APP" refers to pages/functionality intended for authenticated users.
 
@@ -18,6 +27,19 @@ public class AppController {
 	@GetMapping(value = "/dashboard")
 	public String login() {
 		return "dashboard";
+	}
+	
+	@GetMapping("/addproject")
+	public String addNewProjectForm(Model model)
+	{
+		model.addAttribute("project", new Project());
+		return "addproject";
+	}
+	
+	@PostMapping("/addproject")
+	public String addNewProjectSave(@Valid Project project, BindingResult binding, RedirectAttributes redirectAttributes)
+	{
+		return "redirect:project/"+project.getProjId();
 	}
 	
 }
